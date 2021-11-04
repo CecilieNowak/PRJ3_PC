74341bb7dd90d1093debf5d7baa24ba8d0e0d5e9
Soruba Tharumalingam <202005951@post.au.dk>
2021-11-04 10:43:57 +0100

    Kode i datalag og logiklag til tjek af login (ved kalibrering)

---
+++ DataAccessLayer/DataBase.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using DTO_BloodPressureData;
+
+namespace DataAccessLayer
+{
+    public class DataBase : IData
+    {
+        private SqlConnection _connection;
+        private SqlDataReader _reader;
+        private SqlCommand _command;
+        private const String DBlogin = "F21ST2ITS2au669338";
+
+        public DataBase()
+        {}
+
+        public bool IsUserRegistered(String username, String password)
+        {
+            bool result = false;
+
+            _connection = new SqlConnection(@"Data Source=st-i4dab.uni.au.dk;Initial Catalog=" + DBlogin + ";User ID=" + DBlogin + ";Password=" + DBlogin + ";Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            _command = new SqlCommand("select * UsersRegistered where Username ='" + username + "'", _connection);
+            _connection.Open();
+            _reader = _command.ExecuteReader();
+
+            while (_reader.Read())
+            {
+                if (_reader["Username"].ToString() == username && _reader["Keyword"].ToString() == password)
+                {
+                    result = true;
+                    break;
+                }
+            }
+
+            _connection.Close();
+            return result;
+        }
+    }
+
+
+
+
+}
---
+++ DataAccessLayer/IData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DTO_BloodPressureData;
+
+namespace DataAccessLayer
+{
+    public interface IData
+    {
+        bool IsUserRegistered(String socSecNb, String pw);
+    }
+}
