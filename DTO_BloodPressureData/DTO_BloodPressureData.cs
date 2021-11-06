a2a20299e41f6edb76f944f3a3a18480f04f58bb
Abdul1997 <201711212@post.au.dk>
2021-11-06 20:20:46 +0100

    UDP3

--- DTO_BloodPressureData/DTO_BloodPressureData.cs
+++ DTO_BloodPressureData/DTO_BloodPressureData.cs
@@ -2,13 +2,13 @@
 
 namespace DTO_BloodPressureData
 {
-    public class DTO_BloodPressureData
+    public class DTO_BloodpressureData
     {
         public double Systolic { get; private set; }
         public double Diastolic { get; private set; }
         public int Pulse { get; private set; }
 
-        public DTO_BloodPressureData()
+        public DTO_BloodpressureData()
         {
         }
     }
---
+++ DataAccessLayer/UDPListener.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+using DTO_BloodPressureData;
+
+namespace DataAccessLayer
+{
+    class UDPListener
+    {
+
+
+
+        private const int listenPort = 12000;
+        string jsonString;
+
+        public void StartListener()
+        {
+            UdpClient listener = new UdpClient(listenPort);
+            IPEndPoint groupEP = new IPEndPoint(IPAddress.Any, listenPort);
+
+            DTO_BloodpressureData blodData = new DTO_BloodpressureData();
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+                    byte[] bytes = listener.Receive(ref groupEP);
+
+                    jsonString = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+
+                    blodData = JsonSerializer.Deserialize<DTO_BloodpressureData>(jsonString);    // Her tilføjer vi de modtaget data til objektet blodData
+
+                    Console.WriteLine($"Received broadcast from {groupEP} :");      // Her udskriver den groupEp som indholder Senden IP adresse
+
+                    Console.WriteLine("Blood pressure data:");
+
+                    Console.WriteLine("Systolic: " + blodData.Systolic);             // Print den modtaget data fra senden på skæmmen
+                    Console.WriteLine("diastolic: " + blodData.Diastolic);           // Print den modtaget data fra senden på skæmmen
+                    Console.WriteLine("Pulse:" + blodData.Pulse);                    // Print den modtaget data fra senden på skæmmen
+
+                    Console.WriteLine("");                                           // En tom line for at lave en afstand mellem de printet data
+                    Console.WriteLine("");                                           // En tom line for at lave en afstand mellem de printet data
+
+
+
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+            finally
+            {
+                listener.Close();
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+
+    
+}
