9c61d65887c6d2cdfbc5d750409ebac2456571f6
Abdul1997 <201711212@post.au.dk>
2021-11-08 18:22:43 +0100

    h

--- DataAccessLayer/UDPListener.cs
+++ DataAccessLayer/UDPListener.cs
@@ -11,8 +11,6 @@ namespace DataAccessLayer
     class UDPListener
     {
 
-
-
         private const int listenPort = 12000;
         string jsonString;
 
@@ -34,18 +32,13 @@ namespace DataAccessLayer
 
                     blodData = JsonSerializer.Deserialize<DTO_BloodpressureData>(jsonString);    // Her tilføjer vi de modtaget data til objektet blodData
 
-                    Console.WriteLine($"Received broadcast from {groupEP} :");      // Her udskriver den groupEp som indholder Senden IP adresse
-
-                    Console.WriteLine("Blood pressure data:");
-
-                    Console.WriteLine("Systolic: " + blodData.Systolic);             // Print den modtaget data fra senden på skæmmen
-                    Console.WriteLine("diastolic: " + blodData.Diastolic);           // Print den modtaget data fra senden på skæmmen
-                    Console.WriteLine("Pulse:" + blodData.Pulse);                    // Print den modtaget data fra senden på skæmmen
-
-                    Console.WriteLine("");                                           // En tom line for at lave en afstand mellem de printet data
-                    Console.WriteLine("");                                           // En tom line for at lave en afstand mellem de printet data
 
 
+                    // Her skal der måske oprettes blockcollection og så skal data fra blodData tilføjes til det 
+
+
+
+              
 
                 }
             }
