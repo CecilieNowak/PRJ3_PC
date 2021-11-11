f86237ee051d65459e6cdebc4b479c8acf47509c
Soruba Tharumalingam <202005951@post.au.dk>
2021-11-11 10:26:14 +0100

    Dato vises på UI. GUI er tilpasset, så den kan vises på skærmen, uden at skulle trækkes i.

--- DataAccessLayer/DataBase.cs
+++ DataAccessLayer/DataBase.cs
@@ -21,7 +21,7 @@ namespace DataAccessLayer
             _connection = new SqlConnection(@"Data Source=st-i4dab.uni.au.dk;Initial Catalog=" + DBlogin + ";User ID=" + DBlogin + ";Password=" + DBlogin + ";Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             _command = new SqlCommand("select * UsersRegistered where Username ='" + username + "'", _connection);
             _connection.Open();
-            _reader = _command.ExecuteReader();
+           // _reader = _command.ExecuteReader();
 
             while (_reader.Read())
             {
