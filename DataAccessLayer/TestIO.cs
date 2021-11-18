1fe2507a9efcec1499c7344d273508505f971c58
20792@myoffice.site <20792@myoffice.site>
2021-11-18 09:41:54 +0100

    Der kan nu skrives til en fil. Når jeg har skrevet en ting kommer det ind i txt filen, men kører jeg programmet igen, kommer det nye jeg skriver ikke ind.

---
+++ DataAccessLayer/TestIO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Xamarin.Forms;
+
+
+namespace WriteToFile
+{
+    public class TestIO
+    {
+        String text;
+        String text1;
+
+        public void testIO()
+        {
+            Console.Write("Type something: ");
+            text = Convert.ToString(Console.ReadLine());
+            text1 = text;
+            string path = @"Julie.txt";
+
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(text1);
+                    sw.Dispose();
+                }
+            }
+        }
+    }
+}
