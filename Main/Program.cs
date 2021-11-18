cb3b48df9bbfaf4c983a9c2d800bf528fb499647
Andreas Høgh <au639339@uni.au.dk>
2021-11-18 09:42:35 +0100

    CalcBP added

---
+++ Main/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BusinessLogicLayer;
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcBP data = new CalcBP();
+            data.CalcDia();
+            data.CalcDia();
+        }
+    }
+}
--- Test_PRJ3_PC/UnitTest1.cs
+++ Test_PRJ3_PC/UnitTest1.cs
@@ -1,18 +1,34 @@
 using NUnit.Framework;
+using BusinessLogicLayer;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Test_PRJ3_PC
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
+        CalcBP calcBP;
+        
+            [SetUp]
         public void Setup()
         {
+            calcBP = new CalcBP();
+            int s;
         }
 
         [Test]
-        public void Test1()
+        public void CalcSys()
         {
-            Assert.Pass();
+            List<int> data = new List<int> { 1, 5, 4, 3, 2, 5, 7, 8, 4, 2, 1, 4 };
+            List<int> sys = new List<int>();
+            List<int> dia = new List<int>();
+
+            calcBP.CalcDia();
+            calcBP.CalcDia();
+
+            Assert.That(sys.Any(s => s == 8));
+            
         }
     }
 }
