c64353f199104f6cd208666a9608506d2836c273
Andreas Høgh <au639339@uni.au.dk>
2021-11-22 15:36:22 +0100

    alarm update

--- Main/Program.cs
+++ Main/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using BusinessLogicLayer;
 using DataAccessLayer;
+using System.Media;
 
 namespace Main
 {
@@ -9,12 +10,21 @@ namespace Main
     {
         static void Main(string[] args)
         {
-            CalcBP calc = new CalcBP();
-            TestIO test = new TestIO();
-            data.CalcSys();
-            data.CalcDia();
+            //SoundPlayer alarm = new SoundPlayer("sonnette_reveil.wav");
+            //alarm.PlayLooping();
+
+            Alarm a1 = new Alarm();
+            List<int> liste = new List<int> { 1, 1, 1, 1, 1, 5, 5, 5, 5, 5, 1, 11, 1, 6 };
+            a1.StartAlarm(liste);
+
+
+            Console.ReadKey();
+            //CalcBP calc = new CalcBP();
+            //TestIO test = new TestIO();
+            //data.CalcSys();
+            //data.CalcDia();
             
-            test.Test();
+            //test.Test();
 
             //Test af SendToDatabase-klasse
             //SendToDatabase send = new SendToDatabase();
