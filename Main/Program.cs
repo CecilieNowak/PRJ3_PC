2365446cbe7f9ecf0d914bad860de6608bf7bc80
Cecilie Svangtun Nowak <201910286@post.au.dk>
2021-11-22 15:37:18 +0100

    ...

--- Main/Program.cs
+++ Main/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using BusinessLogicLayer;
 using DataAccessLayer;
+using System.Media;
 
 namespace Main
 {
@@ -11,10 +12,15 @@ namespace Main
         {
             CalcBP calc = new CalcBP();
             TestIO test = new TestIO();
-            data.CalcSys();
-            data.CalcDia();
+            //calc.CalcSys(); //den liste med 150 DTO'er i sekundet, skal være parameter til denne metode
+            //calc.CalcDia(); // --||--
             
             test.Test();
+
+            Alarm a1 = new Alarm();
+            List<int> testSys = new List<int> { 1, 2, 3, 45, 23, 23, 24, 25, 1, 2, 3, 45, 23, 23, 24, 25, 1, 2, 3, 45, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, 1, 2, 3, 200, 23, 23, 24, 25, };
+            a1.StartAlarm(testSys);
+            
 
             //Test af SendToDatabase-klasse
             //SendToDatabase send = new SendToDatabase();
---
+++ PresentationLayer/AlarmObserver.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BusinessLogicLayer;
+
+namespace PresentationLayer
+{
+    class AlarmObserver : IBloodPressureObserver
+    {
+
+    }
+}
