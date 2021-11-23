7ab642717a4431482026d978308907a7a9213229
Cecilie Svangtun Nowak <201910286@post.au.dk>
2021-11-23 09:56:50 +0100

    x-akse kalibrering

---
+++ BusinessLogicLayer/getADCvalues.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataAccessLayer;
+
+namespace BusinessLogicLayer
+{
+    public class getADCvalues
+    {
+        
+        public double getADCvaluesFromDataLayer()
+        {
+         ReadADCValues adc = new ReadADCValues();
+            return adc.ReadAdcValues();
+        }
+    }
+}
