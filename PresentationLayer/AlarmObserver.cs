00876b20a8c74ec1b2682c325675afa81a61d04a
Andreas Høgh <au639339@uni.au.dk>
2021-11-23 09:21:09 +0100

    Alarm oprettet som observer

--- PresentationLayer/AlarmObserver.cs
+++ PresentationLayer/AlarmObserver.cs
@@ -2,11 +2,47 @@
 using System.Collections.Generic;
 using System.Text;
 using BusinessLogicLayer;
+using DTO_BloodPressureData;
+using System.Media;
 
 namespace PresentationLayer
 {
     class AlarmObserver : IBloodPressureObserver
     {
 
+        private BloodPressureSubject _bp;
+        private MainWindow mw;
+
+
+        public AlarmObserver(BloodPressureSubject bp, MainWindow mainWindow)
+        {
+            mw = mainWindow;
+            _bp = bp;
+            _bp.Add(this);
+        }
+
+        SoundPlayer alarm = new SoundPlayer("sonnette_reveil.wav");
+        public void Update()                                       
+        { 
+            BloodPressureData subject = _bp.getNewestDTO();
+
+            void StartAlarm(List<int> _bp)
+            {
+                for (int i = 5; i <= _bp.Count - 5; i++)
+                {
+                    if (_bp[i] > 1.3 * _bp[i - 5] || _bp[i] < 0.7 * _bp[i - 5])
+                    {
+                        alarm.PlayLooping();
+
+                    }
+
+                }
+
+            }
+
+
+        }
     }
+
+
 }
